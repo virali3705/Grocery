76442c3e7b180a5b2b1521f8589f66561d132a32
Rajveer <rajveerchauhan936@gmail.com>
2024-03-07 18:12:43 +0530

    Screen move to pages !!

--- lib/widget/Pages/Shipping details.dart
+++ lib/widget/Pages/Shipping details.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:grocery_app/widget/mytimeline.dart';
+import 'package:grocery_app/widget/Pages/mytimeline.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
 class ShippingDetails extends StatelessWidget {
