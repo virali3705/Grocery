3f6e5568cab952119dbb27624166e7c3bb3a4e03
Rajveer <rajveerchauhan936@gmail.com>
2024-03-09 11:48:35 +0530

    My Order Screen's Ui is complete !!

--- lib/Pages/Pages/MyOrder.dart
+++ lib/Pages/Pages/MyOrder.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,7 +10,7 @@ class MyOrders extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: SafeArea(
+        child: SingleChildScrollView(
           child: Column(
             children: [
               Row(
@@ -24,14 +25,405 @@ class MyOrders extends StatelessWidget {
                   ),
                   Padding(
                     padding: const EdgeInsets.only(top: 19,),
-                    child: Text("My Orders",style: GoogleFonts.inter(fontSize: 18),),
+                    child: Text("My Orders",style: GoogleFonts.inter(fontSize: 18,),),
                   ),
                 ],
               ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Center(
+                    child: Container(
+                      height: 55,
+                      width: 300,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Color.fromARGB(255, 229, 229, 229),
+                                spreadRadius: 0.5,
+                                blurRadius: 8),
+                          ]),
+                      child: Row(
+                        children: [
+                    
+                          Row(
+                            children: [
+                              const SizedBox(
+                                width: 15,
+                              ),
+                              const Icon(
+                                Icons.search,
+                                color: Color.fromARGB(255, 50, 227, 56),
+                              ),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: const Text("Search Here",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 17,
+                                        color: Colors.black,
+                                      )))
+                            ],
+                          ),
+                          
+                        ],
+                      ),
+                    ),
+                  ),
+                  Stack(
+                      children: [
+                        Container(
+                        height: 55,
+                        width: 55,
+                        decoration: const BoxDecoration(
+                            color: Color.fromARGB(255, 50, 227, 56),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 3,top: 3),
+                        child: IconButton(onPressed: () {}, icon: const Icon(Icons.filter_alt_outlined,color: Colors.white,size: 35,)),
+                      ),
+                      ],)
+                ],
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              Container(
+                height: 132,
+                width: 369,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Color.fromARGB(255, 229, 229, 229),
+                          spreadRadius: 0.5,
+                          blurRadius: 8),
+                    ]),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    const Image(image: AssetImage("assets/Apple.png"),height: 100,width: 100,),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Delivered by 09th July",
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Lorem Ipsum is simply dummy text of \nthe printing and typesetting industry...",
+                              style: TextStyle(fontSize: 8),
+                            ),
+                          ),
+                           Padding(
+                            padding: const EdgeInsets.only(right:45,bottom: 10),
+                            child: TextButton(onPressed: () {}, child: const Text("Rate Product >>",style: TextStyle(color: Color.fromARGB(255, 50, 227, 56),fontWeight: FontWeight.w600),),),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),  
+              Container(
+                height: 132,
+                width: 369,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Color.fromARGB(255, 229, 229, 229),
+                          spreadRadius: 0.5,
+                          blurRadius: 8),
+                    ]),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    const Image(image: AssetImage("assets/Apple.png"),height: 100,width: 100,),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Delivered by 09th July",
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Lorem Ipsum is simply dummy text of \nthe printing and typesetting industry...",
+                              style: TextStyle(fontSize: 8),
+                            ),
+                          ),
+                           Padding(
+                            padding: const EdgeInsets.only(right:45,bottom: 10),
+                            child: TextButton(onPressed: () {}, child: const Text("Rate Product >>",style: TextStyle(color: Color.fromARGB(255, 50, 227, 56),fontWeight: FontWeight.w600),),),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),Container(
+                height: 132,
+                width: 369,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Color.fromARGB(255, 229, 229, 229),
+                          spreadRadius: 0.5,
+                          blurRadius: 8),
+                    ]),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    const Image(image: AssetImage("assets/Apple.png"),height: 100,width: 100,),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Delivered by 09th July",
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Lorem Ipsum is simply dummy text of \nthe printing and typesetting industry...",
+                              style: TextStyle(fontSize: 8),
+                            ),
+                          ),
+                           Padding(
+                            padding: const EdgeInsets.only(right:45,bottom: 10),
+                            child: TextButton(onPressed: () {}, child: const Text("Rate Product >>",style: TextStyle(color: Color.fromARGB(255, 50, 227, 56),fontWeight: FontWeight.w600),),),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),Container(
+                height: 132,
+                width: 369,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Color.fromARGB(255, 229, 229, 229),
+                          spreadRadius: 0.5,
+                          blurRadius: 8),
+                    ]),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    const Image(image: AssetImage("assets/Apple.png"),height: 100,width: 100,),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Delivered by 09th July",
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Lorem Ipsum is simply dummy text of \nthe printing and typesetting industry...",
+                              style: TextStyle(fontSize: 8),
+                            ),
+                          ),
+                           Padding(
+                            padding: const EdgeInsets.only(right:45,bottom: 10),
+                            child: TextButton(onPressed: () {}, child: const Text("Rate Product >>",style: TextStyle(color: Color.fromARGB(255, 50, 227, 56),fontWeight: FontWeight.w600),),),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),Container(
+                height: 132,
+                width: 369,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Color.fromARGB(255, 229, 229, 229),
+                          spreadRadius: 0.5,
+                          blurRadius: 8),
+                    ]),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    const Image(image: AssetImage("assets/Apple.png"),height: 100,width: 100,),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Delivered by 09th July",
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Lorem Ipsum is simply dummy text of \nthe printing and typesetting industry...",
+                              style: TextStyle(fontSize: 8),
+                            ),
+                          ),
+                           Padding(
+                            padding: const EdgeInsets.only(right:45,bottom: 10),
+                            child: TextButton(onPressed: () {}, child: const Text("Rate Product >>",style: TextStyle(color: Color.fromARGB(255, 50, 227, 56),fontWeight: FontWeight.w600),),),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),Container(
+                height: 132,
+                width: 369,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Color.fromARGB(255, 229, 229, 229),
+                          spreadRadius: 0.5,
+                          blurRadius: 8),
+                    ]),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    const Image(image: AssetImage("assets/Apple.png"),height: 100,width: 100,),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Delivered by 09th July",
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Text(
+                              "Lorem Ipsum is simply dummy text of \nthe printing and typesetting industry...",
+                              style: TextStyle(fontSize: 8),
+                            ),
+                          ),
+                           Padding(
+                            padding: const EdgeInsets.only(right:45,bottom: 10),
+                            child: TextButton(onPressed: () {}, child: const Text("Rate Product >>",style: TextStyle(color: Color.fromARGB(255, 50, 227, 56),fontWeight: FontWeight.w600),),),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
             ],
-          )
-          )    
-      ),
+          ),
+        )
+        ),
     );
   }
 }
--- lib/Pages/Pages/Shipping details.dart
+++ lib/Pages/Pages/Shipping details.dart
@@ -10,17 +10,34 @@ class ShippingDetails extends StatelessWidget {
       body: Center(
         child: SafeArea(
           child: Padding(
+
             padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
             child: ListView(
               children: [
+                const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 20,left: 20
+                    ),
+                    child: Icon(Icons.arrow_back_ios_new_outlined),
+                  ),
+                  SizedBox(
+                    width: 25,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 19,),
+                    child: Text("Profile",style: const TextStyle(fontSize: 18),),
+                  ),
+                ],
+              ),
                 SizedBox(
-                  height: 200,
+                  height: 150,
                   child: TimelineTile(
                     isFirst: true,
                     endChild: const Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(height: 90), // Adjusted height
+                        SizedBox(height: 50), // Adjusted height
                         Padding(
                           padding: EdgeInsets.symmetric(horizontal: 20),
                           child: Text(
@@ -77,10 +94,10 @@ class ShippingDetails extends StatelessWidget {
                   ),
                 ),
                 SizedBox(
-                  height: 100,
+                  height: 200,
                   child: TimelineTile(                      
                     endChild: const Padding(
-                      padding: EdgeInsets.only(top: 40,left: 18),
+                      padding: EdgeInsets.only(top: 100,left: 18),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
@@ -109,7 +126,10 @@ class ShippingDetails extends StatelessWidget {
                           Text(
                               "Your order has been dispatched from july 09 at \n 3:20 PM Ahemdabad HUB",
                               style: TextStyle(fontSize: 12,color: Colors.grey),
-                              )
+                              ),
+                              SizedBox(
+                                height: 50,
+                              ),
                         ],
                       ),
                     ),
@@ -121,12 +141,23 @@ class ShippingDetails extends StatelessWidget {
                   ),
                 ),
                 SizedBox(
-                  height: 500,
+                  height: 550,
                   child: TimelineTile(
                     beforeLineStyle: const LineStyle(color: Colors.green),
                     indicatorStyle: const IndicatorStyle(
                       color: Colors.green,
                       width: 15,
+                    ),
+                    endChild: Column(
+                      children: [
+                        Text("Raj"),
+                        Text("Raj"),
+                        Text("Raj"),
+                        Text("Raj"),
+                        Text("Raj"),
+                        Text("Raj"),
+                        Text("Raj"),
+                      ],
                     ),
                   ),
                 ),
