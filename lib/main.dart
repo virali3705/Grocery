5477083675a36f57e5a78c4a43bceb6e053355a0
virali <viralimakawana@gmail.com>
2024-03-10 01:03:55 -0800

    payment page completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:grocery_app/Pages/Pages/Addaddresses.dart';
@@ -5,14 +7,13 @@ import 'package:grocery_app/Pages/Pages/Categoryscreen.dart';
 import 'package:grocery_app/Pages/Pages/Favouriteproduct.dart';
 import 'package:grocery_app/Pages/Pages/Homescreen.dart';
 import 'package:grocery_app/Pages/Pages/Loginscreen.dart';
-import 'package:grocery_app/Pages/Pages/MyOrder.dart';
-import 'package:grocery_app/Pages/Pages/MyReviews.dart';
 import 'package:grocery_app/Pages/Pages/Productlist.dart';
 import 'package:grocery_app/Pages/Pages/Profile.dart';
 import 'package:grocery_app/Pages/Pages/Register%20screen.dart';
 import 'package:grocery_app/Pages/Pages/Savedaddresses.dart';
-import 'package:grocery_app/Pages/Pages/Shipping%20details.dart';
+//import 'package:grocery_app/Pages/Pages/Shipping%20details.dart';
 import 'package:grocery_app/Pages/Pages/productdetailsscreen.dart';
+import 'package:grocery_app/Payment%20page.dart';
 
 void main() {
   runApp(const FlutterApp());
@@ -30,7 +31,7 @@ class FlutterApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
           useMaterial3: false,
           textTheme: GoogleFonts.montserratTextTheme()),
-      home: MyReviews(),
+      home: const Payment(),
     );
   }
 }
